0f8566f377f43e0ed9faffcc959ee7e6c3eec26a
Thomas Kleist <thomas@kleist-it.com>
2023-12-12 16:55:43 +0100

    init

---
+++ src/test/java/com/danskebankshop/danskebankshop/DanskebankShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danskebankshop.danskebankshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanskebankShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
